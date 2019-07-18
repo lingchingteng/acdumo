61a6f9771f9507e064ca9b6288c088083800aed4
anthony-aylward <aaylward@eng.ucsd.edu>
2019-07-17 18:56:21 -0700

    update

--- acdumo/acdumo.py
+++ acdumo/acdumo.py
@@ -27,29 +27,33 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def get_cookie_crumb():
+def get_cookie_crumb(index=1):
     url = 'https://finance.yahoo.com/lookup?s=bananas'
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
     }
     req = Request(url, headers=headers)
     with urlopen(req) as response:
-        cookie = response.info()['set-cookie'].split(';')[0]
+        cookie = response.info()['Set-Cookie'].split(';')[0]
         response_text = response.read().decode()
         crumb_start = tuple(
             m.end() for m in re.finditer('"crumb":"', response_text)
-        )[-1]
+        )[index]
         crumb = response_text[crumb_start:crumb_start + 11]
+        print(crumb)
         return cookie, crumb, (len(crumb) == 11)
 
 
 def get_yahoo_finance_data(
     stock_ticker: str,
-    crumb: str,
+    crumb_index=8,
+    # cookie: str,
+    # crumb: str,
     start_date: str = '1929-03-08',
     end_date: str = datetime.now().strftime('%Y-%m-%d'),
     frequency: str = 'd'
 ):
+    cookie, crumb, _ = get_cookie_crumb(index=crumb_index)
     ticker_url = ''.join(
         (
             'https://query1.finance.yahoo.com/v7/finance/download/',
@@ -61,13 +65,16 @@ def get_yahoo_finance_data(
             '&crumb=', crumb
         )
     )
-    with urlopen(ticker_url) as response:
+    headers = {'Cookie': cookie}
+    req = Request(ticker_url, headers=headers)
+    with urlopen(req) as response:
         return response.read().decode()
 
 
 def main():
     parse_arguments()
-    result_from_yahoo = get_yahoo_finance_data('SPY')
+    cookie, crumb, valid_cookie_crumb = get_cookie_crumb()
+    result_from_yahoo = get_yahoo_finance_data('SPY', cookie, crumb)
     print(result_from_yahoo)
 
 if __name__ == '__main__':
