4f781da20197eff9b14eb1085ee90cc5848d8ab5
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-07-22 16:47:16 -0700

    config

--- acdumo/auth.py
+++ acdumo/auth.py
@@ -32,3 +32,56 @@ from acdumo.forms import (
 )
 from ucsd_bisb_unofficial.models import get_db, User, Role
 from ucsd_bisb_unofficial.email import send_confirmation_email
+
+
+
+
+# Blueprint assignment =========================================================
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+
+# Functions ====================================================================
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    """Register a new user
+    
+    If the current user is already logged in, they will be redirected to the
+    index page.
+
+    Otherwise, the registration page will be rendered. It includes a
+    RegistrationForm (see `forms.py`).
+    
+    If the supplied email is on the approved email list (and does not already
+    have an account, see models.User), a new user will be created from the form
+    data and added to the database. There it will await confirmation (see
+    `confirm_email`)
+    """
+
+    if current_user.is_authenticated:
+        return redirect(url_for('jumbotron.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        if form.email.data not in current_app.config['APPROVED_EMAILS']:
+            flash('Sorry, that email is not on the approved list.')
+            return redirect(url_for('auth.login'))
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db = get_db()
+        db.create_all()
+        db.session.add(user)
+        user.add_role(Role.query.filter_by(name='named_user').first())
+        db.session.commit()
+        send_confirmation_email(user)
+        flash(
+            'Thanks for registering!  Please check your email to '
+            'confirm your email address.'
+        )
+        return redirect(url_for('auth.login'))
+    return render_template(
+        'auth/register.html',
+        title='Register',
+        form=form
+    )
---
+++ config/__init__.py
@@ -0,0 +1,100 @@
+#!/user/bin/env python3
+#===============================================================================
+# config
+#===============================================================================
+
+"""Create a config file"""
+
+
+
+
+# Imports ======================================================================
+
+import argparse
+import os
+import os.path
+
+
+
+
+# Constants ====================================================================
+
+DEVELOPMENT_CONFIG_DATA = f'''
+import os
+import os.path
+basedir = os.path.abspath(os.path.dirname(__file__))
+SECRET_KEY = os.environ.get('SECRET_KEY') or {os.urandom(16)}
+SQLALCHEMY_DATABASE_URI = (
+    os.environ.get('DATABASE_URL')
+    or 'sqlite:///' + os.path.join(basedir, 'app.db')
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = ''
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ADMINS = ['anthony.aylward@gmail.com']
+APPROVED_EMAILS = ['anthony.aylward@gmail.com']
+'''
+
+PRODUCTION_CONFIG_DATA = f'''
+import os
+import os.path
+basedir = os.path.abspath(os.path.dirname(__file__))
+SECRET_KEY = os.environ.get('SECRET_KEY') or {os.urandom(16)}
+SQLALCHEMY_DATABASE_URI = (
+    os.environ.get('DATABASE_URL')
+    or 'sqlite:///' + os.path.join(basedir, 'app.db')
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'ucsd.bisb.unofficial'
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ADMINS = ['anthony.aylward@gmail.com']
+APPROVED_EMAILS = [
+    'anthony.aylward@gmail.com',
+    'green.danying@gmail.com'
+]
+'''
+
+
+
+
+# Functions ====================================================================
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='write configuration file'
+    )
+    parser.add_argument(
+        'instance',
+        metavar='<path/to/instance-folder/>',
+        help='path to instance folder'
+    )
+    parser.add_argument(
+        '--production',
+        action='store_true',
+        help='write a production config file'
+    )
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+    with open(os.path.join(args.instance, 'config.py'), 'w') as f:
+        f.write(
+            PRODUCTION_CONFIG_DATA if args.production
+            else DEVELOPMENT_CONFIG_DATA
+        )
+
+
+
+
+# Execute ======================================================================
+
+if __name__ == '__main__':
+    main()
