29f512bd1516b897db90dd63ad9ab48bc0d719da
aaylward <aaylward@ucsd-bisb.info>
2019-07-25 06:01:49 +0000

    advance version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='acdumo',
-    version='0.0.1',
+    version='0.0.2',
     author='Anthony Aylward, Serena Wu, Elijah Kun',
     author_email='aaylward@eng.ucsd.edu',
     description='Accelerated dual momentum',
