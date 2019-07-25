0b7850f97797d32e66bf38ea53fef61ccf92aced
anthony-aylward <aaylward@eng.ucsd.edu>
2019-07-25 09:52:45 -0700

    fix header overlap

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='acdumo',
-    version='0.0.12',
+    version='1.0.0',
     author='Anthony Aylward, Serena Wu, Elijah Kun',
     author_email='aaylward@eng.ucsd.edu',
     description='Accelerated dual momentum',
