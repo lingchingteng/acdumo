615bec7f99bae021428fce4af08b069b4253beaf
Anthony Aylward <aaylward@eng.ucsd.edu>
2020-03-13 15:29:22 -0700

    refine

--- acdumo/apscheduler.py
+++ acdumo/apscheduler.py
@@ -4,10 +4,10 @@ from acdumo.email import send_notification_email
 scheduler = APScheduler()
 
 @scheduler.task(
-  'cron',
-  id='send_notification_email',
-  minute='*',
-  misfire_grace_time=900
+    'cron',
+    id='send_notification_email',
+    minute='*',
+    misfire_grace_time=900
 )
 def notification_email():
     send_notification_email()
